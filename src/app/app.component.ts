3f69dea4ad167e384ebddc7cc1dae6e4e3656a9c
Muhammad Nauman <muhammadnauman223@gmail.com>
2025-12-06 19:40:40 +0500

    test: checking hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
         filter(
           (event) => event instanceof RouteConfigLoadStart || event instanceof RouteConfigLoadEnd,
         ),
-        debounceTime(50),
+        debounceTime(50), // prevents flickering flashes
       )
       .subscribe((event) => {
         this.loading.set(event instanceof RouteConfigLoadStart);
